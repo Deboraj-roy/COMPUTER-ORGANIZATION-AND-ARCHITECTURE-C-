81b928fa1d4717f8136cb52f207b85f11d717249
DEBORAJ ROY <61079464+Deboraj-roy@users.noreply.github.com>
2022-03-04 01:14:03 +0600

    Add files via upload

---
+++ Lab01/mycode01.asm
@@ -0,0 +1,23 @@
+;Write two assembly language programs to calculate the following high-level arithmetic instructions and print the result into the screen as output:
+;i) A = B-A-1
+
+
+; The easiest way to print out "Hello, World!"
+
+name "hi"
+
+org 100h
+
+jmp start       ; jump over data declaration
+
+msg:    db      "A = B-A-1", 0Dh,0Ah, 24h
+
+start:  mov     dx, msg  ; load offset of msg into dx.
+        mov     ah, 09h  ; print function is 9.
+        int     21h      ; do it!
+        
+        mov     ah, 0 
+        int     16h      ; wait for any key....
+        
+ret ; return to operating system.
+
