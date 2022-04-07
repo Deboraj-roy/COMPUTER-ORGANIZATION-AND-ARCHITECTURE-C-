4edd8ff6dd9feb215031c36b9dbc6e26442b8a1f
DEBORAJ ROY <61079464+Deboraj-roy@users.noreply.github.com>
2022-04-07 17:22:58 +0600

    Lab 07
    Lab 07 
    
    4/6/2022

---
+++ Lab7/CLEAR bit number 5 and 2 AL(10100110).asm
@@ -0,0 +1,43 @@
+.MODEL  SMALL
+.STACK 100H 
+
+.CODE 
+
+MAIN PROC
+ 
+ 
+    MOV AL,10100110b  
+    MOV BL,11011011b 
+    
+    AND AL,BL
+    
+    MOV BL,AL
+    MOV CX,8
+    
+    PRINT:
+    
+        MOV AH,2
+        MOV DL,'0'
+        TEST BL, 10000000b
+        JZ ZERO
+        
+        MOV DL,'1'
+        
+        ZERO:
+             INT 21H
+             
+        SHL BL,1
+        
+        LOOP PRINT
+     
+    MOV DL,'b'
+    
+    INT 21H
+    
+    ;DOS EXIT
+    
+    MOV AH,4CH
+    INT 21H
+    
+MAIN ENDP
+    END MAIN
